7d6635b8736583073946b57ac08aed21b5b83cb0
beshoymakram <beshoy_makram@icloud.com>
2025-10-16 01:31:41 +0300

    Small fix in navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -59,29 +59,6 @@
       <router-link to="/" class="block border-l-4 border-transparent py-2 pr-4 pl-3 text-base font-medium text-[#2C702C] hover:border-gray-300 hover:bg-gray-50 hover:text-gray-800 sm:pr-6 sm:pl-5 dark:text-gray-300 dark:hover:border-white/20 dark:hover:bg-white/5 dark:hover:text-white">Waste</router-link>
       <router-link to="/" class="block border-l-4 border-transparent py-2 pr-4 pl-3 text-base font-medium text-[#2C702C] hover:border-gray-300 hover:bg-gray-50 hover:text-gray-800 sm:pr-6 sm:pl-5 dark:text-gray-300 dark:hover:border-white/20 dark:hover:bg-white/5 dark:hover:text-white">About Us</router-link>
     </div>
-    <div class="border-t border-gray-200 pt-4 pb-3 dark:border-white/10">
-      <div class="flex items-center px-4 sm:px-6">
-        <div class="shrink-0">
-          <img src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" class="size-10 rounded-full bg-gray-100 outline -outline-offset-1 outline-black/5 dark:bg-gray-800 dark:outline-white/10" />
-        </div>
-        <div class="ml-3">
-          <div class="text-base font-medium text-gray-800 dark:text-white">Tom Cook</div>
-          <div class="text-sm font-medium text-[#2C702C] dark:text-gray-400">tom@example.com</div>
-        </div>
-        <button type="button" class="relative ml-auto shrink-0 rounded-full p-1 text-gray-400 hover:text-[#2C702C] focus:outline-2 focus:outline-offset-2 focus:outline-indigo-600 dark:hover:text-white dark:focus:outline-indigo-500">
-          <span class="absolute -inset-1.5"></span>
-          <span class="sr-only">View notifications</span>
-          <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" data-slot="icon" aria-hidden="true" class="size-6">
-            <path d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" stroke-linecap="round" stroke-linejoin="round" />
-          </svg>
-        </button>
-      </div>
-      <div class="mt-3 space-y-1">
-        <router-link to="/" class="block px-4 py-2 text-base font-medium text-[#2C702C] hover:bg-gray-100 hover:text-gray-800 dark:text-gray-400 dark:hover:bg-white/5 dark:hover:text-white">Your profile</router-link>
-        <router-link to="/" class="block px-4 py-2 text-base font-medium text-[#2C702C] hover:bg-gray-100 hover:text-gray-800 dark:text-gray-400 dark:hover:bg-white/5 dark:hover:text-white">Settings</router-link>
-        <router-link to="/" class="block px-4 py-2 text-base font-medium text-[#2C702C] hover:bg-gray-100 hover:text-gray-800 dark:text-gray-400 dark:hover:bg-white/5 dark:hover:text-white">Sign out</router-link>
-      </div>
-    </div>
   </el-disclosure>
 </nav>
 </template>
