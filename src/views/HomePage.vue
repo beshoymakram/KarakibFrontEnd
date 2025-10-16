38f2d31097ca148ca0432d7dc16140879bb27c43
MaryamMahmoudSayed <maryam00mahmoud@gmail.com>
2025-10-16 20:36:17 +0300

    Add waste carousel section with navigation arrows

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -93,11 +93,103 @@
       </div>
     </div>
   </section>
+
+  <!-- Waste Carousel Section -->
+  <section class="bg-white px-16 py-16 relative">
+    <p class="text-4xl text-[#2C702C] font-semibold text-center">Discover Waste Types</p>
+    <p class="text-3xl text-[#163816] text-center pt-5 pb-10 font-normal">
+      Learn about the different kinds of waste you can recycle
+    </p>
+
+    <!-- Carousel Wrapper -->
+    <div class="relative max-w-6xl mx-auto flex items-center justify-center">
+      <!-- Left Arrow -->
+      <button
+        @click="scrollPrev"
+        class="absolute left-[-50px] top-1/2 -translate-y-1/2 bg-[#EAF2EA] hover:bg-[#d6e7d6] text-[#2C702C] rounded-full p-2 shadow-md z-10"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="2"
+          stroke="currentColor"
+          class="size-6"
+        >
+          <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+        </svg>
+      </button>
+
+      <!-- Carousel (Scrollable Container) -->
+      <div
+        ref="carousel"
+        class="carousel carousel-center bg-white rounded-box space-x-6 p-10 flex overflow-x-auto scroll-smooth"
+      >
+        <div
+          v-for="(item, index) in wasteItems"
+          :key="index"
+          class="carousel-item flex flex-col items-center flex-shrink-0"
+        >
+          <img
+            :src="item.src"
+            :alt="item.name"
+            class="w-[268px] h-[268px] object-cover rounded-full bg-[#EAF2EA]"
+          />
+          <p class="mt-4 text-2xl font-semibold text-[#2C702C]">{{ item.name }}</p>
+        </div>
+      </div>
+
+      <!-- Right Arrow -->
+      <button
+        @click="scrollNext"
+        class="absolute right-[-50px] top-1/2 -translate-y-1/2 bg-[#EAF2EA] hover:bg-[#d6e7d6] text-[#2C702C] rounded-full p-2 shadow-md z-10"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="2"
+          stroke="currentColor"
+          class="size-6"
+        >
+          <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+        </svg>
+      </button>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
   name: "homePage",
+  data() {
+    return {
+      wasteItems: [
+        { name: "Plastic", src: "/public/images/waste/bottles.png" },
+        { name: "Cartons", src: "/public/images/waste/cartons.png" },
+        { name: "Clothes", src: "/public/images/waste/clothes.png" },
+        { name: "Metals", src: "/public/images/waste/metals.png" },
+        { name: "Woods", src: "/public/images/waste/woods.png" },
+        { name: "Cooking Oil", src: "/public/images/waste/cooking-oil.png" },
+        { name: "Home Appliances", src: "/public/images/waste/home-appliances.png" },
+      ],
+    };
+  },
+  methods: {
+    scrollNext() {
+      const carousel = this.$refs.carousel;
+      if (carousel) {
+        carousel.scrollBy({ left: 300, behavior: "smooth" });
+      }
+    },
+
+    scrollPrev() {
+      const carousel = this.$refs.carousel;
+      if (carousel) {
+        carousel.scrollBy({ left: -300, behavior: "smooth" });
+      }
+    },
+  },
 };
 </script>
 
