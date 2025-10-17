72af59e4ffcdbcfb512ce8f53f9b577954a134df
beshoymakram <beshoy_makram@icloud.com>
2025-10-17 16:03:31 +0300

    fix steps design

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -8,7 +8,7 @@
       <div class="hero-overlay bg-white/30"></div>
       <div class="hero-content text-center items-start justify-start pt-14">
         <div class="max-w-3xl">
-          <h1 class="text-4xl text-[#2C702C] font-bold drop-shadow-lg">
+        <h1 class="text-4xl text-[#2C702C] font-bold drop-shadow-lg">
             Turn Your Waste Into Worth
           </h1>
           <p class="py-6 text-md font-medium text-[#439a43]">
@@ -16,8 +16,8 @@
             rewards — because sustainability should pay off.
           </p>
           <button
-            class="btn rounded-md cursor-pointer bg-[#2C702C] px-3 py-2 text-sm font-semibold text-white shadow-xs focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#2C702C] hover:bg-[#265C26]"
-          >
+            class="btn rounded-md cursor-pointer bg-[#2C702C] px-3 py-2 text-sm font-semibold text-xl text-white shadow-xs focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#2C702C] hover:bg-[#265C26]"
+   text-lg        >
             Get Started
           </button>
         </div>
@@ -26,67 +26,67 @@
 
     <!-- Steps Section -->
     <section class="px-16 py-12 bg-base-200">
-      <div class="flex flex-wrap justify-between gap-8">
+      <div class="flex flex-wrap justify-center gap-8">
         <!-- Step 1 -->
         <div class="card bg-base-100 shadow-sm w-[290px] rounded-[50px]">
-          <figure class="p-4">
+          <figure class="p-4 pb-0">
             <img
               src="/images/step-1.png"
               alt="step-1"
-              class="w-[290px] h-[271px] object-cover rounded-[50px]"
+              class="w-full h-60 object-cover rounded-[50px]"
             />
           </figure>
           <div class="card-body text-center">
-            <h2 class="font-semibold text-[#317C31]">Step 1</h2>
-            <p class="card-title">Create Account</p>
+            <h2 class="font-semibold text-xl text-[#317C31]">1</h2>
+            <p class="font-semibold text-lg">Create Account</p>
             <p>Sign up to become a member of our recycling community.</p>
           </div>
         </div>
 
         <!-- Step 2 -->
         <div class="card bg-base-100 shadow-sm w-[290px] rounded-[50px]">
-          <figure class="p-4">
+          <figure class="p-4 pb-0">
             <img
               src="/images/step-2.png"
               alt="step-2"
-              class="w-[290px] h-[271px] object-cover rounded-[50px]"
+              class="w-full h-60 object-cover rounded-[50px]"
             />
           </figure>
           <div class="card-body text-center">
-            <h2 class="font-semibold text-[#317C31]">Step 2</h2>
-            <p class="card-title">Collect Waste</p>
+            <h2 class="font-semibold text-xl text-[#317C31]">2</h2>
+            <p class="font-semibold text-lg">Collect Waste</p>
             <p>Sort and store your recyclables to prepare them for pickup.</p>
           </div>
         </div>
 
         <!-- Step 3 -->
         <div class="card bg-base-100 shadow-sm w-[290px] rounded-[50px]">
-          <figure class="p-4">
+          <figure class="p-4 pb-0">
             <img
               src="/images/step-3.png"
               alt="step-3"
-              class="w-[290px] h-[271px] object-cover rounded-[50px]"
+              class="w-full h-60 object-cover rounded-[50px]"
             />
           </figure>
           <div class="card-body text-center">
-            <h2 class="font-semibold text-[#317C31]">Step 3</h2>
-            <p class="card-title">Request Pickup</p>
+            <h2 class="font-semibold text-xl text-[#317C31]">3</h2>
+            <p class="font-semibold text-lg">Request Pickup</p>
             <p>Schedule a collection through the app for convenient service.</p>
           </div>
         </div>
 
         <!-- Step 4 -->
         <div class="card bg-base-100 shadow-sm w-[290px] rounded-[50px]">
-          <figure class="p-4">
+          <figure class="p-4 pb-0">
             <img
               src="/images/step-4.png"
-              alt="step-4"
-              class="w-[290px] h-[271px] object-cover rounded-[50px]"
+              alt="stepx-4"
+              class="w-full h-60 object-cover rounded-[50px]"
             />
           </figure>
           <div class="card-body text-center">
-            <h2 class="font-semibold text-[#317C31]">Step 4</h2>
-            <p class="card-title">Earn Rewards</p>
+            <h2 class="font-semibold text-xl text-[#317C31]">4</h2>
+            <p class="font-semibold text-lg">Earn Rewards</p>
             <p>Get rewarded for your effort with points and exclusive benefits.</p>
           </div>
         </div>
