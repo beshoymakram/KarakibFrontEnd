63a89b24dfbd2e148e34e59ec0199e8bdd025092
ahmad hisham <ahmadhisham5555@gmail.com>
2025-10-18 16:53:47 +0300

    updated cart logic

--- src/stores/cart.js
+++ src/stores/cart.js
@@ -2,7 +2,8 @@ import { defineStore } from 'pinia'
 
 export const useCartStore = defineStore('cart', {
   state: () => ({
-    cartItems: [],
+    cartItems: JSON.parse(localStorage.getItem('cartItems')) || [],
+    checkoutData: JSON.parse(localStorage.getItem('checkoutData')) || null,
   }),
 
   getters: {
@@ -11,21 +12,46 @@ export const useCartStore = defineStore('cart', {
   },
 
   actions: {
-    addToCart(product) {
-      const existingItem = this.cartItems.find(item => item.id === product.id)
+    addToCart(product, type) {
+      const existingItem = this.cartItems.find(item => item.id === product.id && item.type === type)
       if (existingItem) {
         existingItem.quantity++
       } else {
-        this.cartItems.push({ ...product, quantity: 1 })
+        this.cartItems.push({ ...product, type, quantity: 1 })
       }
+      this.saveToLocalStorage()
     },
 
-    removeFromCart(productId) {
-      this.cartItems = this.cartItems.filter(item => item.id !== productId)
+    removeFromCart(productId, type) {
+      this.cartItems = this.cartItems.filter(item => !(item.id === productId && item.type === type))
+      this.saveToLocalStorage()
     },
 
     clearCart() {
       this.cartItems = []
+      localStorage.removeItem('cartItems')
+    },
+
+    getCheckoutData() {
+      const merchItems = this.cartItems
+        .filter(item => item.type === 'merch')
+        .map(({ id, quantity }) => ({ id, quantity }))
+
+      const wasteItems = this.cartItems
+        .filter(item => item.type === 'waste')
+        .map(({ id, quantity }) => ({ id, quantity }))
+
+      return { merchItems, wasteItems }
+    },
+
+    saveCheckoutData() {
+      const data = this.getCheckoutData()
+      this.checkoutData = data
+      localStorage.setItem('checkoutData', JSON.stringify(data))
+    },
+
+    saveToLocalStorage() {
+      localStorage.setItem('cartItems', JSON.stringify(this.cartItems))
     },
   },
 })
