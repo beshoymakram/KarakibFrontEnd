74aa92ad51c56c62e5b07d390576633a286ee70f
beshoymakram <beshoy_makram@icloud.com>
2025-11-17 03:21:35 +0200

    Withdrawals

--- src/views/Admin/Withdrawals.vue
+++ src/views/Admin/Withdrawals.vue
@@ -93,16 +93,16 @@
             </td>
             <td class="px-6 py-4 whitespace-nowrap text-sm space-x-2 mx-auto items-center justify-center flex gap-2">
               <button v-if="transaction.status !== 'cancelled' && transaction.status !== 'completed'"
-                @click="openCompleteModal(order)"
+                @click="openCompleteModal(transaction)"
                 class="px-3 py-1 border border-green-300 rounded-md text-primary hover:bg-green-50 transition-colors cursor-pointer">
                 {{ $t('common.complete') }}
               </button>
-              <button @click="openDetailsModal(order)"
+              <button @click="openDetailsModal(transaction)"
                 class="px-3 py-1 border border-gray-300 rounded-md text-primary hover:bg-gray-100 transition-colors">
                 {{ $t('common.details') }}
               </button>
               <button v-if="transaction.status !== 'cancelled' && transaction.status !== 'completed'"
-                @click="openRefundModal(order)"
+                @click="openRefundModal(transaction)"
                 class="  px-3 py-1 border border-red-300 rounded-md text-red-600 hover:bg-red-50 transition-colors cursor-pointer">
                 {{ $t('common.refund') }}
               </button>
@@ -115,7 +115,75 @@
   </div>
 
   <Teleport to="body">
+    <div v-if="showRefundModal"
+      class="fixed inset-0 z-50 flex items-center justify-center overflow-y-auto overflow-x-hidden bg-black/30"
+      @click="showRefundModal = false">
+      <div class="relative p-4 w-full max-w-md" @click.stop>
+        <div class="relative bg-white rounded-lg shadow">
+          <button type="button" @click="showRefundModal = false"
+            class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center">
+            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+            </svg>
+            <span class="sr-only">Close modal</span>
+          </button>
+          <div class="p-4 md:p-5 text-center">
+            <svg class="mx-auto mb-4 text-gray-400 w-12 h-12" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+              fill="none" viewBox="0 0 20 20">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+            </svg>
+            <h3 class="mb-5 text-lg font-normal text-gray-500">
+              {{ $t('common.areYouSureRefundWithdraw') }}
+            </h3>
+            <button @click="confirmRefundWithdraw" type="button"
+              class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
+              {{ $t("common.YesSure") }}
+            </button>
+            <button @click="showRefundModal = false" type="button"
+              class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 focus:z-10 focus:ring-4 focus:ring-gray-100 ">
+              {{ $t('common.No, Cancel') }}
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
 
+    <div v-if="showCompleteModal"
+      class="fixed inset-0 z-50 flex items-center justify-center overflow-y-auto overflow-x-hidden bg-black/30"
+      @click="showCompleteModal = false">
+      <div class="relative p-4 w-full max-w-md" @click.stop>
+        <div class="relative bg-white rounded-lg shadow">
+          <button type="button" @click="showCompleteModal = false"
+            class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center">
+            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+            </svg>
+            <span class="sr-only">Close modal</span>
+          </button>
+          <div class="p-4 md:p-5 text-center">
+            <svg class="mx-auto mb-4 text-gray-400 w-12 h-12 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+              fill="none" viewBox="0 0 20 20">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+            </svg>
+            <h3 class="mb-5 text-lg font-normal text-gray-500">
+              {{ $t('common.areYouSureCompleteWithdraw') }}
+            </h3>
+            <button @click="confirmCompleteWithdraw" type="button"
+              class="text-white bg-[#2C702C] hover:bg-[#1a4d1a] focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+              {{ $t("common.YesSure") }}
+            </button>
+            <button @click="showCompleteModal = false" type="button"
+              class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 focus:z-10 focus:ring-4 focus:ring-gray-100">
+              {{ $t('common.No, Cancel') }}
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   </Teleport>
 
 </template>
@@ -131,9 +199,9 @@ export default {
   data() {
     return {
       searchQuery: '',
-      selectedType: '',
-      showCreateModal: false,
-      showDonateModal: false,
+      selectedWithdraw: '',
+      showRefundModal: false,
+      showCompleteModal: false,
       createForm: {
         id: null,
         amount: '',
@@ -191,20 +259,39 @@ export default {
   },
 
   methods: {
-    openWithdrawModal() {
-      this.showDonateModal = true;
+    openRefundModal(withdraw) {
+      this.selectedWithdraw = withdraw.id;
+      this.showRefundModal = true;
     },
+
+    openCompleteModal(withdraw) {
+      this.selectedWithdraw = withdraw.id;
+      this.showCompleteModal = true;
+    },
+
 
     async confirmCompleteWithdraw() {
       try {
-        const response = await withdrawalsService.completeWithdraw(this.createForm);
+        const response = await withdrawalsService.completeWithdraw(this.selectedWithdraw);
         nextTick(() => {
           this.$toast.success(response.data.message);
         });
-        this.showDonateModal = false;
-        this.createForm.amount = '';
-        this.createForm.wallet_number = '';
-        useAuthStore().fetchUser();
+        this.showCompleteModal = false;
+        this.selectedWithdraw = '';
+        this.fetchWithdrawals();
+      } catch (error) {
+        this.$toast.error(error.response.data.message);
+      }
+    },
+
+    async confirmRefundWithdraw() {
+      try {
+        const response = await withdrawalsService.refundWithdraw(this.selectedWithdraw);
+        nextTick(() => {
+          this.$toast.success(response.data.message);
+        });
+        this.showRefundModal = false;
+        this.selectedWithdraw = '';
         this.fetchWithdrawals();
       } catch (error) {
         this.$toast.error(error.response.data.message);
