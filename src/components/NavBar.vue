548db098d3cb86657f4b0615de130fdded1dca0c
ahmad hisham <ahmadhisham5555@gmail.com>
2025-11-24 00:36:54 +0200

    fixing light mode icon

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -134,11 +134,8 @@
           <button @click="toggleTheme" class=" rounded-full transition-all duration-300 cursor-pointer hover:scale-120"
             aria-label="Toggle theme">
             <!-- Light Mode Icon -->
-            <svg v-if="currentTheme === 'forest'" xmlns="http://www.w3.org/2000/svg"
-              class="w-5 h-5 m:w-6 sm:h-6 md:w-7 md:h-7 text-yellow-400" fill="none" viewBox="0 0 24 24"
-              stroke="currentColor" stroke-width="2">
-              <path stroke-linecap="round" stroke-linejoin="round"
-                d="M12 3v1m0 16v1m8.485-8.485l-.707.707M4.222 4.222l.707.707M21 12h1M2 12H1m16.97 6.97l.707.707M4.222 19.778l.707-.707M12 8a4 4 0 100 8 4 4 0 000-8z" />
+            <svg v-if="currentTheme === 'forest'" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-sun w-5 h-5 m:w-6 sm:h-6 md:w-7 md:h-7 text-yellow-500" viewBox="0 0 16 16">
+              <path d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6m0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8M8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0m0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13m8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5M3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8m10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0m-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0m9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707M4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708"/>
             </svg>
 
             <!-- Dark Mode Icon -->
