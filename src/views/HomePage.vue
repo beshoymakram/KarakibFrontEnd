fee084fd9392f1633d3566dac92f7f2aa2654800
Maryam Mahmoud Sayed <maryam00mahmoud@gmail.com>
2025-11-25 10:10:09 +0200

    review stars at cards

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -193,6 +193,21 @@
           </div>
         </div>
 
+
+        <div class="flex flex-col items-center gap-0">
+          <div class="flex items-center gap-3 md:gap-4 mb-1 lg:mb-2">
+
+                <div class="flex">
+                  <svg v-for="i in 5" :key="i" class="w-2.5 h-2.5 sm:w-3.5 sm:h-3.5"
+                    :class="i <= Math.floor(productRating) ? 'text-yellow-400' : 'text-gray-300'" fill="currentColor"
+                    viewBox="0 0 20 20">
+                    <path
+                      d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                  </svg>
+                </div>
+
+
+            </div>
         <!-- Out of stock button -->
         <button v-if="product.stock < 1"
           class="btn rounded-md bg-gray-500 text-white cursor-not-allowed px-2 md:px-4  md:py-2 text-xs sm:text-sm font-semibold"
@@ -211,7 +226,7 @@
           class="btn  border-0 rounded-md bg-[#2C702C] text-white hover:bg-[#265C26] px-2 md:px-4  md:py-2 text-xs sm:text-sm font-semibold">
           {{ $t("common.selectSizeFirst") }}
         </router-link>
-
+</div>
 
       </div>
     </div>
