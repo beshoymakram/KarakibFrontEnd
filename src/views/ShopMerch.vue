a820eb31013b76a40e3f70adc4a473a11ad68019
beshoymakram <beshoy_makram@icloud.com>
2025-11-30 01:05:54 +0200

    Rating on product card

--- src/views/ShopMerch.vue
+++ src/views/ShopMerch.vue
@@ -74,9 +74,17 @@
             </h2>
             <p class="hidden md:block text-gray-500 text-xs sm:text-sm">{{ product.description }}</p>
           </div>
-            <p class="text-primary font-semibold text-xs sm:text-sm md:text-base lg:text-lg " >
-              {{ product.price }} {{ $t("common.currency") }}
-            </p>
+          <div class="flex">
+            <svg v-for="i in 5" :key="i" class="w-2.5 h-2.5 sm:w-3.5 sm:h-3.5"
+              :class="i <= Math.floor(productRating(product)) ? 'text-yellow-400' : 'text-gray-300'" fill="currentColor"
+              viewBox="0 0 20 20">
+              <path
+                d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+            </svg>
+          </div>
+          <p class="text-primary font-semibold text-xs sm:text-sm md:text-base lg:text-lg ">
+            {{ product.price }} {{ $t("common.currency") }}
+          </p>
 
         </div>
       </router-link>
@@ -167,13 +175,13 @@ export default {
 
       return filtered;
     },
-  cartMap() {
-  const map = {};
-  this.cartStore.products.forEach(item => {
-    map[item.cartable_id] = item;
-  });
-  return map;
-}
+    cartMap() {
+      const map = {};
+      this.cartStore.products.forEach(item => {
+        map[item.cartable_id] = item;
+      });
+      return map;
+    }
 
   },
   methods: {
@@ -225,6 +233,10 @@ export default {
       } catch (error) {
         this.$toast.error(error.response.data.message);
       }
+    },
+
+    productRating(product) {
+      return Number(product.average_rating).toFixed(1)
     },
   },
   mounted() {
